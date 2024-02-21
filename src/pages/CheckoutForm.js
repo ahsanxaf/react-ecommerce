386523eb87d14f39637bd2251796f93e45ae4f67
Ahsan Farooq <ahsanfarooqxx69@gmail.com>
2024-02-21 22:25:03 +0500

    test commit

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -60,7 +60,7 @@ export default function CheckoutForm() {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: `http://localhost:3000/order-success/${currentOrder.id}`,
+        return_url: `/order-success/${currentOrder.id}`,
       },
     });
 
